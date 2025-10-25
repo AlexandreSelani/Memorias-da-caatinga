3922876c9e10d28fb7e870b28c1e02a101dfdc49
Alexandre Selani <xandyselani@outlook.com>
2025-10-25 14:37:52 -0300

    fade in e out na morte

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Fade : MonoBehaviour
+{   
+    public Canvas componenteDeUI; //componente geral de ui
+    private GameObject painel_de_fade; //painel que realiza fade in/out
+    public float duracao_fade_in; 
+    public float duracao_fade_out;
+    private bool UIAtivo = true;
+
+    //flags usadas para garantir que os fade-in/out vao ter sido concluidos
+    private bool flag = false;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        painel_de_fade = componenteDeUI.transform.Find("Fade_in-out")?.gameObject;
+        painel_de_fade.SetActive(UIAtivo);
+        StartCoroutine(fadeout_sequence());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public IEnumerator fadeout_sequence()
+    {   
+        UIAtivo = true;
+        painel_de_fade.SetActive(UIAtivo);
+        yield return StartCoroutine(fadeout());
+        UIAtivo = false;
+        painel_de_fade.SetActive(UIAtivo);
+    }
+
+    public IEnumerator fadein_sequence()
+    {   
+        UIAtivo = true;
+        painel_de_fade.SetActive(UIAtivo);
+        yield return StartCoroutine(fadein());
+        
+    }
+
+    private IEnumerator fadeout()
+    {
+        Image fundoDoPainel = painel_de_fade.GetComponent<Image>();
+        Color cor_painel = fundoDoPainel.color;
+        float alpha_inicial_painel = cor_painel.a;
+        float alpha_final_painel = 0f;
+        float tempo = 0f;
+
+
+        while (tempo < duracao_fade_out)
+        {
+            cor_painel.a = Mathf.Lerp(alpha_inicial_painel, alpha_final_painel, tempo / duracao_fade_out); // interpolação suave
+            fundoDoPainel.color = cor_painel;
+            tempo += Time.deltaTime;
+            yield return null;
+        }
+
+        cor_painel.a = alpha_final_painel;                  // garante alfa final
+        fundoDoPainel.color = cor_painel;
+
+
+    }
+    
+    private IEnumerator fadein()
+    {
+
+        Image fundoDoPainel = painel_de_fade.GetComponent<Image>();
+        Color cor_painel = fundoDoPainel.color;
+        float alpha_inicial_painel = cor_painel.a;
+        float alpha_final_painel = 255f / 255f;
+
+
+        float tempo = 0f;
+        
+
+        while (tempo < duracao_fade_in)
+        {
+            cor_painel.a = Mathf.Lerp(alpha_inicial_painel, alpha_final_painel, tempo / duracao_fade_in); // interpolação suave
+            fundoDoPainel.color = cor_painel;
+            tempo += Time.deltaTime;
+            yield return null;
+        }
+
+        cor_painel.a = alpha_final_painel;                  // garante alfa final
+        fundoDoPainel.color = cor_painel;
+
+    }
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,31 +1,35 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using System.Collections;
 public class PlayerHealth : MonoBehaviour
 {
     public int life = 1;
     public float respawnDelay = 1.0f; // segundos antes de reiniciar cena
     public string killTag = "Kill";
-
+    private Fade fade;
     private bool dead = false;
 
+    void Start()
+    {
+        fade = GetComponent<Fade>();
+    }
     
     private void OnTriggerEnter(Collider other)
     {
         if (dead) return;
         if (other.CompareTag(killTag))
         {
-            Die();
+            StartCoroutine(Die());
         }
     }
 
-    void Die()
+    public IEnumerator Die()
     {
         dead = true;
         // aqui: parar controle do jogador
         //var controller = GetComponent<PlayerController>(); // se tiver um script de controle
         //if (controller != null) controller.enabled = false;
-
+        StartCoroutine(fade.fadein_sequence());
         // animação/som (opcional)
         AudioSource deathSound = gameObject.GetComponent<AudioSource>();
         if (deathSound != null) deathSound.Play();
@@ -34,6 +38,8 @@ public class PlayerHealth : MonoBehaviour
         var rend = GetComponentInChildren<Renderer>();
         if (rend != null) rend.enabled = false;
 
+
+        yield return new WaitForSeconds(0.5f);
         // reinicia cena após delay
         Invoke(nameof(RestartScene), respawnDelay);
     }
